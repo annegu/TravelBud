f683b5f4f793ef05a67417a07b298b7d9cdb1878
Ben <benjamin.smieja@gmail.com>
2021-01-16 12:47:45 -0500

    add dynamic buttons

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-COURSES = []
+COURSES = ["ece216","ece221","ece231","ece243","ece216","ece221","ece231","ece243"]
 
 
-@app.route("/")
+@app.route("/",  methods=["POST", "GET"])
 def homepage():
     """View function for Home Page."""
-    return render_template("coursePage.html", courseName = "Ece216")
+
+    return render_template("coursePage.html", courseName = "Ece216",Courses = COURSES)
 
 @app.route("/signup", methods=["POST", "GET"])
 def signup():
     form = SignUpForm()
     return render_template("signup.html", form = form)
 
+
+@app.route("/changePage",  methods=["POST", "GET"])
+def changePage():
+    """View function for Home Page."""
+
+    return render_template("coursePage.html", courseName = "IT WORKEDDD")
 class StudentCourseData:
   def __init__(self):
     self.labsCompleted = []
@@ -27,7 +34,6 @@ class Assignment:
         self.id = id
         self.name = name
         self.dueDate = dueDate
-        self.worth = worth
         self.classRating = 0
         self.numDone = 0
 
