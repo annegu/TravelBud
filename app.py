80c39eb02236272aff1cb377d328fd9ad554378c
Ben <benjamin.smieja@gmail.com>
2021-01-16 08:50:57 -0500

    add basic structure for flask

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def homepage():
+    """View function for Home Page."""
+    return render_template("home.html", pets = pets)
+
+
+
+
+if __name__ == "__main__":
+    app.run()#debug=True
