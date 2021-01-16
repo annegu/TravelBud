9b99cdc111666c0c4d55e8299aa96a4cded97250
kebinshorts <59887703+kebinshorts@users.noreply.github.com>
2021-01-16 16:54:04 -0500

    signup and login pages
    signup and login pages

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-
-
 @app.route("/")
 def homepage():
     """View function for Home Page."""
     return render_template("home.html")
 
+@app.route("/add-course")
+def createcourse():
+    """View function for Creare Course Page."""
+    return render_template("add-course.html")
 
+@app.route("/signup")
+def signup():
+    """View function for Creare Course Page."""
+    return render_template("signup.html")
+
+@app.route("/login")
+def login():
+    """View function for Creare Course Page."""
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
